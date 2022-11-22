20f6aa44d882497c5a0e6dae4c0cd4124344ed8a
SVA2021 <sofronov.vit@gmail.com>
2022-11-22 19:56:55 +0400

    add expired date visialisation

--- src/components/TodoListItem/TodoListItem.tsx
+++ src/components/TodoListItem/TodoListItem.tsx
@@ -1,14 +1,25 @@
 import classNames from 'classnames';
+import {FC} from 'react';
 import styles from './TodoListItem.module.scss';
 import {TodoListItemProps} from './TodoListItem.type';
 
-export const TodoListItem = ({item, checkHandler, editHandler, deleteHandler}: TodoListItemProps) => {
+/**
+ * @param item: TodoItem, main data of todo task
+ * @param checkHandler: void, callback for changing status of todo task
+ * @param editHandler: void, callback for starting edit todo task
+ * @param deleteHandler: void, callback for deleting of todo task
+ * @returns JSX component
+ */
+
+export const TodoListItem: FC<TodoListItemProps> = ({item, checkHandler, editHandler, deleteHandler}) => {
 
   function withoutDefault(event: React.MouseEvent, func: () => void) {
     event.stopPropagation();
     event.preventDefault();
     func();
   }
+
+  const isDateOver = new Date().getTime() < new Date(item.expired).getTime();
 
   return (
     <div
@@ -20,7 +31,10 @@ export const TodoListItem = ({item, checkHandler, editHandler, deleteHandler}: T
     >
       <h3 className={styles.TodoItem__header}> {item.title} </h3>
       <p className={styles.TodoItem__description}>{item.description}</p>
-      <p className={styles.TodoItem__description}>expired date: {item.expired}</p>
+      <p className={styles.TodoItem__description}>
+        expired date: {item.expired}
+        {isDateOver && <span className={styles.TodoItem__overdate} >!!!Date is over!!!</span>}
+      </p>
       {item.filePath &&
         <p className={styles.TodoItem__description}>attached file:
           <a className={styles.TodoItem__link} href={item.filePath} target={'_blank'} rel={'noopener noreferrer'}>
